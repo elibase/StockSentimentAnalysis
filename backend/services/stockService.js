997f39737322a115a6dfc0e053d6dc6a498a71fa
elibase <elibask@yahoo.ca>
2026-05-06 01:14:07 -0400

    added mapping for response data in stockService

--- backend/services/stockService.js
+++ backend/services/stockService.js
@@ -8,13 +8,24 @@ async function getStockTimeSeries(ticker) {
     const url = `${BASE_URL}function=TIME_SERIES_DAILY&datatype=json&symbol=${ticker}&apikey=${API_KEY}`
     const response = await axios.get(url);
 
-  const data = response.data;
+    const data = response.data;
 
     if (!data || !data["Time Series (Daily)"]) {
       throw new Error("Invalid response from Alpha Vantage");
     }
 
-    return data["Time Series (Daily)"];
+    const timeSeries = data["Time Series (Daily)"];
+
+    return Object.entries(timeSeries)
+      .map(([date, values]) => ({
+        date,
+        open: Number(values["1. open"]),
+        high: Number(values["2. high"]),
+        low: Number(values["3. low"]),
+        close: Number(values["4. close"]),
+        volume: Number(values["5. volume"]),
+      }))
+      .sort((a, b) => new Date(a.date) - new Date(b.date));
   } catch (error) {
     console.error("Error fetching prices:", error.message);
     throw new Error("Failed to fetch time series data");
